e132d32da80ab5784bf6187f8bf6f0a2eb243e72
VinceyZhang <38546959+VinceyZhang@users.noreply.github.com>
2018-06-07 17:54:51 +0800

    配置~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`

---
+++ src/main/java/com/permission/common/SpringExceptionResolver.java
@@ -0,0 +1,38 @@
+package com.permission.common;
+
+import com.permission.exception.PermissionException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class SpringExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object o, Exception ex) {
+        String url = request.getRequestURL().toString();
+        ModelAndView mv;
+        String defaultMsg = "System error";
+
+        // .json, .page
+        if (url.endsWith(".json")) {
+            if (ex instanceof PermissionException) {
+                JsonData result = JsonData.fail(ex.getMessage());
+                mv = new ModelAndView("jsonView", result.toMap());
+            } else {
+                log.error("unknow exception,");
+                JsonData result = JsonData.fail(defaultMsg);
+                mv = new ModelAndView("jsonView", result.toMap());
+            }
+        } else if (url.endsWith(".page")) {
+            JsonData result = JsonData.fail(defaultMsg);
+            mv = new ModelAndView("exception", result.toMap());
+        } else {
+            JsonData result = JsonData.fail(defaultMsg);
+            mv = new ModelAndView("jsonView", result.toMap());
+        }
+        return mv;
+    }
+}
